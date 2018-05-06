c99256860539a1bbcc6a480d187c28b0660b210f
stefland <stephan.maachi@gmail.com>
2018-05-06 14:58:16 +0200

    app
    pb in user-dashboard.component.ts

---
+++ app/material.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatToolbarModule],
+
+  exports: [
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatToolbarModule]
+})
+export class MaterialModule {
+}
---
+++ app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from "@angular/router";
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    RouterModule
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    MaterialModule,
+    ReactiveFormsModule,
+  ],
+  declarations: []
+})
+export class SharedModule { }
